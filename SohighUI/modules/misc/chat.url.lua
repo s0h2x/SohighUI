533e62c55ed46e54d40758e0c2b728be75e57adc
sohigh <33549022+sativahigh@users.noreply.github.com>
2021-05-18 01:37:27 +1000

    fix tip in chat

--- SohighUI/modules/misc/chat.url.lua
+++ SohighUI/modules/misc/chat.url.lua
@@ -57,3 +57,75 @@
 		local addmessage = frame.AddMessage
 		frame.AddMessage = function(self, text, ...) addmessage(self, ReURL_AddLinkSyntax(text), ...) end
 	end
+
+	--// Multi ItemRefTooltip
+	local tips = {[1] = _G['ItemRefTooltip']}
+	local types = {item = true, enchant = true, spell = true, quest = true, unit = true, talent = true, currency = true}
+
+	local CreateTip = function(link)
+		for k, v in ipairs(tips) do
+			for i, tip in ipairs(tips) do
+				if tip:IsShown() and tip.link == link then
+					tip.link = nil
+					HideUIPanel(tip)
+					return
+				end
+			end
+			if not v:IsShown() then
+				v.link = link
+				return v
+			end
+		end
+
+		local num = #tips + 1
+		local tip = CreateFrame('GameTooltip', 'ItemRefTooltip'..num, UIParent, 'GameTooltipTemplate')
+		tip:StripLayout(true)
+		tip:SetLayout()
+		tip:SetShadow()
+		tip:SetAnchor('BOTTOM', 0, 80)
+		tip:SetSize(128, 64)
+		tip:EnableMouse(true)
+		tip:SetMovable(true)
+		tip:SetClampedToScreen(true)
+		tip:RegisterForDrag('LeftButton')
+		tip:SetScript('OnDragStart', function(self) self:StartMoving() end)
+		tip:SetScript('OnDragStop', function(self) self:StopMovingOrSizing() end)
+		tip:HookScript('OnShow', function(self)
+			self:SetBackdropColor(1,1,1)
+			self:SetBackdropBorderColor(1,1,1)
+		end);
+
+		local closeButton = CreateFrame('Button', 'ItemRefTooltip'..num..'CloseButton', tip)
+		closeButton:SetAnchor('TOPRIGHT')
+		closeButton:CloseTemplate()
+		closeButton:SetScript('OnClick', function(self) HideUIPanel(tip) end);
+
+		table.insert(UISpecialFrames, tip:GetName())
+
+		tip.link = link
+		tips[num] = tip
+
+		return tip
+	end
+
+	local ShowTip = function(tip, link)
+		ShowUIPanel(tip)
+		if not tip:IsShown() then
+			tip:SetOwner(UIParent, 'ANCHOR_PRESERVE')
+		end
+		tip:SetHyperlink(link)
+	end
+
+	local _SetItemRef = SetItemRef
+	function SetItemRef(...)
+		local link, text, button = ...
+		local handled = strsplit(':', link)
+		if not IsModifiedClick() and handled and types[handled] then
+			local tip = CreateTip(link)
+			if tip then
+				ShowTip(tip, link)
+			end
+		else
+			return _SetItemRef(...)
+		end
+	end
